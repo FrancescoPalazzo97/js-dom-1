96d23c5b202c3254145806819a0b60c7bd2c2973
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-25 15:52:47 +0100

    [BONUS] Bonus completato

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ const lampOn = document.getElementById('lamp-on'); // Mi vado a prendere l'immag
 const lampOff = document.getElementById('lamp-off'); // Mi vado a prendere l'immagine tramite ID
 const btn = document.querySelector('.btn'); // Mi vado a prendere il button tramite classe
 
+console.log(btn.innerText)
+
 btn.addEventListener('click', ()=>{
-    lampOff.className = 'd-none'; // Tramite click sul button l'immagine della lampadina spenta diventa display: none
-    lampOn.className = 'd-block'; // Invece la lampadina accesa diventa display: block
+    if (lampOff.className === 'd-block') {
+        lampOff.className = 'd-none'; // Tramite click sul button l'immagine della lampadina spenta diventa display: none
+        lampOn.className = 'd-block'; // Invece la lampadina accesa diventa display: block
+        // btn.innerText = `Accendi`
+        btn.innerText = 'Spegni'
+    }
+    else {
+        lampOff.className = 'd-block'; // Tramite click sul button l'immagine della lampadina spenta diventa display: block
+        lampOn.className = 'd-none'; // Invece la lampadina accesa diventa display: none
+        btn.innerText = 'Accendi'
+    }
 } )
