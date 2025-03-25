78c07dd5d12e799856966069e97f253b6e5d9071
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-25 15:56:59 +0100

    [BONUS] Bonus completato utilizzando .includes()

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ const lampOn = document.getElementById('lamp-on'); // Mi vado a prendere l'immag
 const lampOff = document.getElementById('lamp-off'); // Mi vado a prendere l'immagine tramite ID
 const btn = document.querySelector('.btn'); // Mi vado a prendere il button tramite classe
 
-console.log(btn.innerText)
-
+// btn.addEventListener('click', ()=>{
+//     if (lampOff.className === 'd-block') {
+//         lampOff.className = 'd-none'; // Tramite click sul button l'immagine della lampadina spenta diventa display: none
+//         lampOn.className = 'd-block'; // Invece la lampadina accesa diventa display: block
+//         // btn.innerText = `Accendi`
+//         btn.innerText = 'Spegni'
+//     }
+//     else {
+//         lampOff.className = 'd-block'; // Tramite click sul button l'immagine della lampadina spenta diventa display: block
+//         lampOn.className = 'd-none'; // Invece la lampadina accesa diventa display: none
+//         btn.innerText = 'Accendi'
+//     }
+// } )
+// ALTERNATIVA USANDO .INCLUDES()
 btn.addEventListener('click', ()=>{
-    if (lampOff.className === 'd-block') {
+    if (lampOff.className.includes(`d-block`)) {
         lampOff.className = 'd-none'; // Tramite click sul button l'immagine della lampadina spenta diventa display: none
         lampOn.className = 'd-block'; // Invece la lampadina accesa diventa display: block
         // btn.innerText = `Accendi`
