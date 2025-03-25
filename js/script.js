529dd909d6ce86837ed96358ecd270fed9d925e1
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-25 15:37:37 +0100

    [UPD] Definizione variabili e assegnazione

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const lampOn = document.getElementById('lamp-on'); // Mi vado a prendere l'immagine tramite ID
+const lampOff = document.getElementById('lamp-off'); // Mi vado a prendere l'immagine tramite ID
+const btn = document.querySelector('.btn'); // Mi vado a prendere il button tramite classe
