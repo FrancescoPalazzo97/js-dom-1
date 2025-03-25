71ac013267cf33edb4418fee5092c848caaed618
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-03-25 15:44:20 +0100

    [DONE] Esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 const lampOn = document.getElementById('lamp-on'); // Mi vado a prendere l'immagine tramite ID
 const lampOff = document.getElementById('lamp-off'); // Mi vado a prendere l'immagine tramite ID
 const btn = document.querySelector('.btn'); // Mi vado a prendere il button tramite classe
+
+btn.addEventListener('click', ()=>{
+    lampOff.className = 'd-none'; // Tramite click sul button l'immagine della lampadina spenta diventa display: none
+    lampOn.className = 'd-block'; // Invece la lampadina accesa diventa display: block
+} )
